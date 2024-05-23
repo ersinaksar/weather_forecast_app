0b5ee1beefd1cc668f58a1b8a1702daacf760534
ersinaksar <ersinaksar@yandex.com>
2024-05-23 12:19:54 +0300

    feat(weather_app): add WeatherData serializer and viewset

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.utils import timezone
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import WeatherData
+from .serializers import WeatherDataSerializer
+from .utils import fetch_weather_data
+from datetime import timedelta
+from django.conf import settings
 
-# Create your views here.
+
+class WeatherDataViewSet(viewsets.ViewSet):
+    def list(self, request):
+        lat = request.query_params.get('lat')
+        lon = request.query_params.get('lon')
+        data_type = request.query_params.get('data_type')
+
+        if not all([lat, lon, data_type]):
+            return Response({'error': 'Missing required parameters'}, status=400)
+
+        try:
+            weather_data = WeatherData.objects.get(latitude=lat, longitude=lon, data_type=data_type)
+            if timezone.now() - weather_data.timestamp > timedelta(minutes=settings.WEATHER_DATA_TIMEOUT):
+                raise WeatherData.DoesNotExist
+        except WeatherData.DoesNotExist:
+            weather_data = fetch_weather_data(lat, lon, data_type)
+            WeatherData.objects.update_or_create(
+                latitude=lat, longitude=lon, data_type=data_type,
+                defaults={'data': weather_data, 'timestamp': timezone.now()}
+            )
+            weather_data = WeatherData.objects.get(latitude=lat, longitude=lon, data_type=data_type)
+
+        serializer = WeatherDataSerializer(weather_data)
+        return Response(serializer.data)
