25697dde7075994280fce0ca4351004cdc214d1b
ersinaksar <ersinaksar@yandex.com>
2024-05-23 13:54:05 +0300

    test(weather_app): update tests to use One Call API 3.0

--- weather_app/tests.py
+++ weather_app/tests.py
@@ -1,17 +1,16 @@
-from django.test import TestCase, override_settings
+from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 
 class WeatherDataTests(TestCase):
     def setUp(self):
         self.client = APIClient()
+        load_dotenv()
 
-    @override_settings(OPENWEATHER_API_KEY=os.getenv('OPENWEATHER_API_KEY'))
     def test_get_weather_data(self):
         url = reverse('weather-list')
         response = self.client.get(url, {'lat': 33.441792, 'lon': -94.037689, 'data_type': 'current'})
