c0a64818ff38835ac60d67a55e625f053bbbaef5
ersinaksar <ersinaksar@yandex.com>
2024-05-23 12:18:23 +0300

    feat(weather_app): add WeatherData model and migration

--- weather_app/models.py
+++ weather_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class WeatherData(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    data_type = models.CharField(max_length=50)
+    data = models.JSONField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('latitude', 'longitude', 'data_type')
