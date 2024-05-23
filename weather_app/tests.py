f5caa1381554f71095661fb224c706f6d1e933fa
ersinaksar <ersinaksar@yandex.com>
2024-05-23 12:22:48 +0300

    test(weather_app): add test for fetching weather data

--- weather_app/tests.py
+++ weather_app/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class WeatherDataTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_get_weather_data(self):
+        url = reverse('weather-list')
+        response = self.client.get(url, {'lat': 33.441792, 'lon': -94.037689, 'data_type': 'current'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
