7975f5922c90f29670736ade942ad43e6354cdea
ersinaksar <ersinaksar@yandex.com>
2024-05-23 13:53:55 +0300

    feat(weather_app): update weather data fetching to use One Call API 3.0

--- weather_app/utils.py
+++ weather_app/utils.py
@@ -11,14 +11,24 @@ load_dotenv()
 
 def fetch_weather_data(lat, lon, data_type):
     api_key = os.getenv('OPENWEATHER_API_KEY')  # get the API key from.env file
-    url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=hourly,daily&appid={api_key}"
+    if not api_key:
+        raise Exception("API key not found. Make sure to set it in the .env file.")
+    exclude = {
+        'current': 'minutely,hourly,daily',
+        'minutely': 'current,hourly,daily',
+        'hourly': 'current,minutely,daily',
+        'daily': 'current,minutely,hourly'
+    }.get(data_type, '')
+    url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={exclude}&appid={api_key}"
 
     logger.info(f"Fetching weather data from URL: {url}")
     response = requests.get(url)
     logger.info(f"Response status code: {response.status_code}")
     logger.info(f"Response content: {response.content}")
+    print(response.json())
 
     if response.status_code != 200:
+        logger.error('Error fetching data from OpenWeatherMap')
         raise Exception('Error fetching data from OpenWeatherMap')
 
     data = response.json()
